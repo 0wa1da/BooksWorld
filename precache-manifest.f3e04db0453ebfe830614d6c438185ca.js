80224513086481478fec3f6cfc19298c337878f1
Your Name <you@example.com>
2020-09-12 18:58:53 +0300

    Updates

---
+++ precache-manifest.f3e04db0453ebfe830614d6c438185ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ee4904fbc866eceada32f77e2281346",
+    "url": "/BooksWorld/index.html"
+  },
+  {
+    "revision": "727d83fc9593bca1b3e2",
+    "url": "/BooksWorld/static/css/main.66b884fd.chunk.css"
+  },
+  {
+    "revision": "5b0322e90ca7cea7f621",
+    "url": "/BooksWorld/static/js/2.a68b9c06.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/BooksWorld/static/js/2.a68b9c06.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "727d83fc9593bca1b3e2",
+    "url": "/BooksWorld/static/js/main.87ddc908.chunk.js"
+  },
+  {
+    "revision": "b09436ae5ecd6a8cdb2b",
+    "url": "/BooksWorld/static/js/runtime-main.0059b016.js"
+  }
+]);
